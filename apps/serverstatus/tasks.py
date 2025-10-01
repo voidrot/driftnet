a62ceaa66636c47ea47ce12134c4cfa4317f9d28
Buck Brady <bbrady145@gmail.com>
2025-09-30 22:17:44 -0600

    fix(celery): update task routing for server status to use correct naming conventions feat(serverstatus): enhance fetch_server_status task with error handling and retry logic style(template): add spacing in current players template for improved readability style(tailwind): add countdown styles for better display of countdown elements

--- apps/serverstatus/tasks.py
+++ apps/serverstatus/tasks.py
@@ -2,23 +2,30 @@ import logging
 
 from celery import shared_task
 
+from apps.esi.exceptions import HTTPCacheError
 from apps.serverstatus.models import ServerStatus
 from apps.shared.providers import esi
 
 logger = logging.getLogger(__name__)
 
 
-@shared_task()
-def fetch_server_status():
+@shared_task(bind=True)
+def fetch_server_status(self):
     """Query the EVE Online server status endpoint."""
 
-    logger.debug('Fetching server status from ESI')
-    op = esi.client.Status.GetStatus()
-    response = op.result()
-    logger.debug('Server status response: %s', response)
-    ServerStatus(
-        player_count=response.players,
-        server_version=response.server_version,
-        start_time=response.start_time,
-        vip_mode=response.vip if response.vip is not None else False,
-    ).save()
+    try:
+        logger.debug('Fetching server status from ESI')
+        op = esi.client.Status.GetStatus()
+        response = op.result()
+        logger.debug('Server status response: %s', response)
+        ServerStatus(
+            player_count=response.players,
+            server_version=response.server_version,
+            start_time=response.start_time,
+            vip_mode=response.vip if response.vip is not None else False,
+        ).save()
+    except HTTPCacheError as e:
+        logger.exception(
+            'Failed getting cached data for 304 Not Modified Response: %s', e
+        )
+        raise self.retry(exc=e, countdown=60, max_retries=3) from e
